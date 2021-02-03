c1dcbe6d157714d9c6167001f0aa929b15ae2efa
IJlnflhbrzDev <IJlnflhbrzDev@gmail.com>
2021-02-04 00:40:39 +0700

    membuat strukture folder React Ap

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import "./Styless/Style.css"
+
 import reportWebVitals from './reportWebVitals';
+import Home from './container/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
